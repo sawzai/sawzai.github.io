07bb18453a76399bf088b489c16db58c05ecc242
SawZai <jinhuang.saw@srglobal.com>
2025-02-18 12:56:13 +0800

    v-html="item"

--- components/Ticket.20241209.js
+++ components/Ticket.20241209.js
@@ -22,6 +22,9 @@ export default {
         <ul v-if="ticket.included" class="category-items">
           <li v-for="(item, index) in ticket.includedArray" :key="index" :class="{ 'active': index < ticket.initems }">{{ item }}</li>
         </ul>
+        <ul v-if="ticket.included" class="category-items">
+          <li v-for="(item, index) in ticket.includedArray" :key="index" :class="{ 'active': index < ticket.initems }" v-html="item">{{ item }}</li>
+        </ul>
         <a v-if="!ticket.soldout === 'FALSE' || ticket.thibuttonon === 'TRUE'" class="ticket-btn3" :href="ticket.thiurl">
           <div class="category-btnsub">{{ ticket.thibuttonsub }}</div>
           <div class="category-btntitle">{{ ticket.thibutton }}</div>
