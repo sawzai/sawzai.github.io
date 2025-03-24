07953b520563bc38594ca23a7886a61edc816826
SawZai <jinhuang.saw@srglobal.com>
2025-03-24 12:24:57 +0800

    update manual change

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = Vue.createApp({
   </section>
 
   <!-- <Modal class="container" textline="Replace Modal Textline" /> -->
-
-  <Tickets jsonfile="https://raw.githubusercontent.com/sawzai/sawzai.github.io/main/assets/data.json" />
+  <teleport to="#SRTickets">  
+  <Tickets jsonfile="https://sawzai.github.io/json/2025/nac2508sg-partner.json?t=${Date.now()}" />
+  </teleport>
 
   <teleport to="#app2">
     <CountDown style="padding-block:60px" class="container" 
@@ -24,6 +25,33 @@ const app = Vue.createApp({
       message: 'Welcome to SawZai Components',
     };
   },
+  mounted() {
+    this.observeTicketChanges();
+  },
+  // methods: {
+  //   observeTicketChanges() {
+  //     const observer = new MutationObserver((mutationsList, observer) => {
+  //       mutationsList.forEach(mutation => {
+  //         const updateItem = document.querySelector('.category-yellow .category-price');
+  //         if (updateItem) {
+  //           updateItem.innerHTML = `S$100`;
+  //           observer.disconnect();
+  //         }
+  //         const updateItem2 = document.querySelector('.category-silver .category-price');
+  //         if (updateItem2) {
+  //           updateItem2.innerHTML = `S$50`;
+  //           observer.disconnect();
+  //         }
+  //       });
+  //     });
+  //     const targetNode = document.getElementById("SRTickets");
+  //     if (targetNode) {
+  //       observer.observe(targetNode, { childList: true, subtree: true });
+  //     } else {
+  //       console.warn("No Item Found to Update");
+  //     }
+  //   }
+  // },
   components: { Tickets, Modal, SRFooterSG, CountDown },
 }).mount('#app');
 
