317a51f66708e0de52cf1dbd7d92c9b339c6c4c0
Atcharlucky <atcharlucky.s@srglobal.com>
2025-04-15 10:38:20 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -4,7 +4,7 @@ export { default as SRFooter } from './SRFooter-uk.js';
 export { default as SRFooterMY } from './SRFooter-MY.js';
 export { default as SRFooterUK } from './SRFooter-uk.js';
 export { default as SRFooterSG } from './SRFooter-SG.js';
-export { default as SRFooterSGTK } from './SRFooter-SG-TK.js';
+export { default as SRFooterSGMYTK } from './SRFooter-SGMY-TK.js';
 export { default as SRFooterEU } from './SRFooter-EU.js';
 export { default as SRFooterSA } from './SRFooter-SA.js';
 export { default as SRFooterSATK } from './SRFooter-SA-TK.js';
