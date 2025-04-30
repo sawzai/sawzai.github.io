294eab870aa60a56ff4e55e3fc7cc102a3683cda
SawZai <jinhuang.saw@srglobal.com>
2025-04-30 11:35:58 +0800

    update

--- components/TicketsFull.js
+++ components/TicketsFull.js
@@ -68,12 +68,12 @@ export default {
   },
 
   mounted() {
-    const cssFile = this.theme === 'dark' ? '../assets/tickets.dark.css' : '../assets/tickets.css';
+    const cssFile = this.theme === 'dark' ? 'https://sawzai.github.io/assets/tickets.dark.css' : 'https://sawzai.github.io/assets/tickets.css';
 
     Promise.all([
-      loadScript('../srcart/js/srthrivecart-min.js', 'srCart'),
+      loadScript('https://sawzai.github.io/srcart/js/srthrivecart-min.js', 'srCart'),
       loadCSS(cssFile, 'tickets-css'),
-      loadCSS('../assets/font-awesome.min.css', 'awesome-css')
+      loadCSS('https://sawzai.github.io/assets/font-awesome.min.css', 'awesome-css')
     ])
     .then(() => {
       this.fetchTickets();
