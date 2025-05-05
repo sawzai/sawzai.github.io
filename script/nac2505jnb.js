a7f24baf0df97a7ed7e8a0e15b3f88924351ba75
SawZai <jinhuang.saw@srglobal.com>
2025-05-05 17:08:40 +0800

    .json included

--- script/nac2505jnb.js
+++ script/nac2505jnb.js
@@ -1,7 +1,7 @@
 const scriptTag = document.getElementById('srapp');
 const baseJsonfile = scriptTag ? scriptTag.dataset.json : '';
 
-const jsonfile = `https://sawzai.github.io/json/2025/${baseJsonfile}?t=${Date.now()}`;
+const jsonfile = `https://sawzai.github.io/json/2025/${baseJsonfile}.json?t=${Date.now()}`;
 
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js';
 import { TicketsFull, SRFooter, SalesPopup } from '../components/index.js';
