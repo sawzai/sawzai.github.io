088674321f5f5cf931106017b8d9871555adfeb0
SawZai <jinhuang.saw@srglobal.com>
2025-05-05 15:39:03 +0800

    example nac2505jnb single file

---
+++ script/nac2505jnb.js
@@ -0,0 +1,25 @@
+const scriptTag = document.querySelector('script[data-jsonfile]');
+const baseJsonfile = scriptTag ? scriptTag.dataset.jsonfile : '';
+
+const jsonfile = `https://sawzai.github.io/json/${baseJsonfile}?t=${Date.now()}`;
+
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js';
+import { TicketsFull, SRFooter, SalesPopup } from 'https://sawzai.github.io/components/index.js';
+
+const app = createApp({
+  template: `
+      <SRFooter country="SA" />
+      <teleport to="#SRTickets">
+        <TicketsFull theme="default" ref="ticketsComponent" :jsonfile="jsonfile" />
+      </teleport>
+     <!-- <SalesPopup :tickets="['1','2','3','4']" :categories="['Silver','Gold','Diamond']" /> -->
+    `,
+  data() {
+    return {
+      jsonfile
+    };
+  },
+  components: { SRFooter, TicketsFull, SalesPopup }
+})
+
+app.mount('#SRFooter');
